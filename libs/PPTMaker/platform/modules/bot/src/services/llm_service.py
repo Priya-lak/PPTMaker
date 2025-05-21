81e99e4881c312513ded995385cf7fb75c0b5c46
Priya Lakhani <priya.lakhani@kevit.io>
2025-05-21 15:57:06 +0530

    llm service setup

---
+++ libs/PPTMaker/platform/modules/bot/src/services/llm_service.py
@@ -0,0 +1,42 @@
+from typing import Dict,List
+from groq import Groq
+from libs.utils.common.custom_logger import CustomLogger
+from libs.PPTMaker.platform.modules.bot.src.config.groq_config import (
+    MODEL_NAME,
+    MAX_TOKENS,
+    GROQ_API_KEY
+)
+
+log = CustomLogger("GroqLLMService", is_request=False)
+
+logger, listener = log.get_logger()
+listener.start()
+
+
+class LLMService:
+    def __init__(self):
+        self.client = Groq(api_key=GROQ_API_KEY)
+        self.model = MODEL_NAME
+        self.max_tokens = MAX_TOKENS
+
+    @staticmethod
+    def build_user_message(message):
+        return {"role": "user", "content": message}
+
+    def call_llm(self, messages:List[Dict[str,str]]):
+        response = self.client.chat.completions.create(
+            messages=messages, model=self.model, max_tokens=self.max_tokens
+        )
+        return response.choices[0].message.content
+
+
+def main():
+    llm_service = LLMService()
+    user_input = "Hi! How are you"
+    llm_message = LLMService.build_user_message(user_input)
+    response = llm_service.call_llm([llm_message])
+    print(f"LLM response {response}")
+
+
+if __name__ == "__main__":
+    main()
