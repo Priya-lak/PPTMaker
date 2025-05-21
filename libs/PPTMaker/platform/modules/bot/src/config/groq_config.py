755fc57cf4a77d2f8807512854c101486e2dd823
Priya Lakhani <priya.lakhani@kevit.io>
2025-05-21 15:57:06 +0530

    llm service setup

---
+++ libs/PPTMaker/platform/modules/bot/src/config/groq_config.py
@@ -0,0 +1,20 @@
+from os import path
+
+from dotenv import dotenv_values
+
+env_path = ".env"
+
+if not path.exists(env_path):
+    raise Exception(".env file not found")
+
+config = dotenv_values(env_path)
+
+GROQ_API_KEY = config["GROQ_API_KEY"]
+
+
+MODEL_NAME = config.get(
+    "MODEL_NAME", "meta-llama/llama-4-maverick-17b-128e-instruct"
+)
+MAX_TOKENS = int(config.get(
+    "MAX_TOKENS", 2000
+))
